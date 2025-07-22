f57eca857dd7a22cd219ca9af6ede489b7d5c2ba
Tanaka humbah <tanakahumbah@gmail.com>
2025-07-22 03:03:27 +0200

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  SESSION_ID: "malvin~8vlFCAjC#v1u0n2jX6YYW0gJJbSFxQF4qQFnBUmSTpuYFp5l16tg ",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "263786453367", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
