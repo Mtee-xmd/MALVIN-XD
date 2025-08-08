bd30650d9110db306f2ee22b8f00b81a593d8ca4
Tanaka humbah <tanakahumbah@gmail.com>
2025-08-09 01:33:18 +0200

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~IyFVkAxQ#dk9gV5RLm2pUfFxD4xej5fnpoHesXnBn_Ihu7TN5f1Q",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "263786453367", // put your phone number here
   
-  PREFIX: ".", // prefix (e.g., ., /, !, *)
+  PREFIX: "+", // prefix (e.g., ., /, !, *)
   
   TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
 };
